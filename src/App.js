8ddd35934e3e7d8bbcf87255dff8f22ff55b7b50
mnxmnz <codi0703@naver.com>
2020-12-28 21:09:27 +0900

    favicon 생성 및 사용하지 않는 파일 삭제

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>소희 민지 예자 주희 파이팅!</p>
+        <p>소희 민지 예지 주희 파이팅!</p>
       </header>
     </div>
   );
