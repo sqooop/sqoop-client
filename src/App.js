d33e38c1743cfc6567f68c7073f02c732ce07ef5
Hyun-juhee <juhe1998@naver.com>
2020-12-28 11:08:04 +0900

    develop 브랜치 생성

--- src/App.js
+++ src/App.js
@@ -2,17 +2,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>소희 민지 예자 주희 파이팅!</p>
       </header>
     </div>
   );
