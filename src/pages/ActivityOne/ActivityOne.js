516fd384d0c87353a3a48fe22f60436f50124701
mnxmnz <codi0703@naver.com>
2021-01-05 02:03:51 +0900

    activity one 뷰 레이아웃 수정

--- src/pages/ActivityOne/ActivityOne.js
+++ src/pages/ActivityOne/ActivityOne.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
 import ActivityOneTitle from '../../components/ActivityOne/ActivityOneTitle';
-import PhotoUpload from '../../components/common/PhotoUpload';
+import PhotoUpload from '../../components/ActivityOne/PhotoUpload';
 import DefaultData from '../../components/createActivity/DefaultData';
 import DateSelector from '../../containers/createActivity/DateSelector.container';
 import Group from '../../containers/createActivity/Group.container';
 import JobTagSelector from '../../containers/common/JobTagSelector.container';
 import SkillTagSelector from '../../containers/common/SkillTagSelector.container';
 import Summary from '../../containers/createActivity/Summary.container';
-import FileUpload from '../../components/common/FileUpload';
+import FileUpload from '../../components/ActivityOne/FileUpload';
 import ActivityOneUserData from '../../components/ActivityOne/ActivityOneUserData';
 
 const ActivityOne = () => {
@@ -16,7 +16,8 @@ const ActivityOne = () => {
     <StyledActivityOneWrap>
       <ActivityOneTitle />
       <LeftWrap>
-        <StyledActivityUserData>
+        <PhotoUpload />
+        <StyledActivityUserData style={{ marginTop: '7px' }}>
           <ContentWrap>
             <DefaultData text="활동 기간" isStar={true} />
             <DateSelector />
@@ -52,27 +53,25 @@ const ActivityOne = () => {
 };
 
 const StyledActivityOneWrap = styled.div`
-  padding-left: 11.438rem;
-  padding-right: 13.438rem;
-
-  @media screen and (max-width: 868px) {
-    padding-left: 6.438rem;
-    padding-right: 6.438rem;
-  }
+  padding-left: 18.3rem;
+  padding-right: 18.3rem;
 `;
 
 const StyledActivityUserData = styled.div``;
 
 const ContentWrap = styled.div`
-  margin-top: 10px;
+  margin-top: 15px;
 `;
 
 const LeftWrap = styled.div`
   float: left;
+  width: 35.8rem;
 `;
 
 const RightWrap = styled.div`
   float: left;
+  width: 53.2rem;
+  position: relative;
 `;
 
 export default ActivityOne;
