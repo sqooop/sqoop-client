f9b74bdc5c135d2e55ed8d15e70566bcf982e99b
mnxmnz <codi0703@naver.com>
2021-01-04 14:29:23 +0900

    package.json 설정 변경

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import activity from './modules/activity';
 import jobTag from './modules/jobTag';
 import skillTag from './modules/skillTag';
+import useranswer from './modules/useranswer';
 
 const rootReducer = combineReducers({
   activity,
   jobTag,
   skillTag,
+  useranswer,
 });
 
 export default rootReducer;
