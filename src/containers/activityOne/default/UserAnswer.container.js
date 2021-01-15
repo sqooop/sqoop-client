d11ef909f1ff02f63694f1a913c4eb27d10b4187
Hyun-juhee <juhe1998@naver.com>
2021-01-15 23:48:45 +0900

    update: activityOne 수정

--- src/containers/activityOne/default/UserAnswer.container.js
+++ src/containers/activityOne/default/UserAnswer.container.js
@@ -1,18 +1,14 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { setUseranswer, setQuestion } from '../../../store/modules/userdata';
+import React from 'react';
+import { useSelector } from 'react-redux';
 import UserData from '../../../components/activityOne/default/UserData';
-import getOneActivity from '../../../lib/api/activityOne/basic';
 
 const UserAnswerContainer = () => {
-  const dispatch = useDispatch();
   const answer = useSelector(state => state.userdata.useranswer);
   const question = useSelector(state => state.userdata.question);
-
   return (
     <>
       {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(idx => (
-        <UserData answer={answer.idx} question={question[idx]} />
+        <UserData answer={answer[idx]} question={question[idx]} />
       ))}
     </>
   );
