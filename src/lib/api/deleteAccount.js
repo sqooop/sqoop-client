11c0e7bf2ff60d9fed967c480b1d3ed3fbf5d4f3
mnxmnz <codi0703@naver.com>
2021-03-04 00:59:41 +0900

    chore: 배포 버전을 위해 console.log 제거

--- src/lib/api/deleteAccount.js
+++ src/lib/api/deleteAccount.js
@@ -2,9 +2,9 @@ import axios from 'axios';
 const baseURL = 'https://sqoop.ga/';
 
 export const postDeleteAccount = async ({ reasonData, passwordData }) => {
-  console.log(reasonData);
-  console.log(passwordData);
-  console.log(localStorage.getItem('token'));
+  // console.log(reasonData);
+  // console.log(passwordData);
+  // console.log(localStorage.getItem('token'));
 
   return await axios.post(
     `${baseURL}user/deleteAccount`,
